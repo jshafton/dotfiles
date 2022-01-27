ad16fcd6a9adb0ef72e126e17f2c6012d633745e
Jacob Shafton <jacob@shafton.com>
2022-01-27 08:30:35 -0700

    handle ssh in kitty terminal

--- shellrc/rc.d.osx/aliases.osx.sh
+++ shellrc/rc.d.osx/aliases.osx.sh
@@ -1,16 +1,6 @@
-alias s="subl ."
+#! /usr/bin/env sh
 
-# Flush Directory Service cache
-alias flush="sudo killall -HUP mDNSResponder"
-
-# Show/hide hidden files in Finder
-alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
-alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
-
-# Hide/show all desktop icons (useful when presenting)
-alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
-alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
-
-# Merge PDF files
-# Usage: `mergepdf -o output.pdf input{1,2,3}.pdf`
-alias mergepdf='/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py'
+if [ "$TERM" = "xterm-kitty" ]; then
+  # https://wiki.archlinux.org/title/Kitty#Terminal_issues_with_SSH
+  alias ssh="kitty +kitten ssh"
+fi
