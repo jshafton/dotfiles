8f69d0e3be505249cbfa3f79ff94c00445e3ad89
Jacob Shafton <jacob@shafton.com>
2022-02-23 11:22:58 -0700

    use tmux-next if available in remote sessions

--- shellrc/rc.d.osx/ssh.osx.sh
+++ shellrc/rc.d.osx/ssh.osx.sh
@@ -1,12 +1,23 @@
 #! /usr/bin/env sh
 
+SCRIPT=$(cat <<'EOF'
+  if hash tmux-next >/dev/null; then
+    tmux="tmux-next"
+  else
+    tmux="tmux"
+  fi
+
+  $tmux new -A -s remote
+EOF
+)
+
 if [ "$TERM" = "xterm-kitty" ]; then
   # https://wiki.archlinux.org/title/Kitty#Terminal_issues_with_SSH
   ssh() {
-    kitty +kitten ssh -t "$@" tmux new -A -s remote
+    kitty +kitten ssh -t "$@" "$SCRIPT"
   }
 else
   ssh() {
-    /usr/bin/ssh -t "$@" tmux new -A -s remote
+    /usr/bin/ssh -t "$@" "$SCRIPT"
   }
 fi
