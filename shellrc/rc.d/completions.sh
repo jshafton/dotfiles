e73e543bb4967024d50b9daad94b2aada5bdf8ba
Jacob Shafton <jacob@shafton.com>
2023-09-18 09:46:32 -0600

    tmuxinator completion

--- shellrc/rc.d/completions.sh
+++ shellrc/rc.d/completions.sh
@@ -1,2 +1,27 @@
-# Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
-[ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2 | tr ' ' '\n')" scp sftp ssh
+#!/usr/bin/env bash
+
+if command -v tmuxinator > /dev/null; then
+  _tmuxinator() {
+      COMPREPLY=()
+      local word
+      word="${COMP_WORDS[COMP_CWORD]}"
+
+      if [ "$COMP_CWORD" -eq 1 ]; then
+          local commands="$(compgen -W "$(tmuxinator commands)" -- "$word")"
+          local projects="$(compgen -W "$(tmuxinator completions start)" -- "$word")"
+
+          COMPREPLY=( $commands $projects )
+      elif [ "$COMP_CWORD" -eq 2 ]; then
+          local words
+          words=("${COMP_WORDS[@]}")
+          unset words[0]
+          unset words[$COMP_CWORD]
+          local completions
+          completions=$(tmuxinator completions "${words[@]}")
+          COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+      fi
+  }
+
+  complete -F _tmuxinator tmuxinator mux
+  alias mux="tmuxinator"
+fi;
