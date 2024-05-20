afb05f293303816c420bf86b155f92c8ec83ca3e
Jacob Shafton <jacob@shafton.com>
2024-05-20 16:09:42 +0000

    de-brew direnv hook

--- shellrc/rc.d/_direnv.sh
+++ shellrc/rc.d/_direnv.sh
@@ -2,12 +2,12 @@
 
 if hash direnv >/dev/null 2>&1; then
   _direnv_hook() {
-    local previous_exit_status=$?;
-    trap -- '' SIGINT;
-    eval "$("/opt/homebrew/bin/direnv" export bash)";
-    trap - SIGINT;
-    return $previous_exit_status;
-  };
+    local previous_exit_status=$?
+    trap -- '' SIGINT
+    eval "$("direnv" export bash)"
+    trap - SIGINT
+    return $previous_exit_status
+  }
   if ! [[ "${PROMPT_COMMAND:-}" =~ _direnv_hook ]]; then
     PROMPT_COMMAND="_direnv_hook${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
   fi
