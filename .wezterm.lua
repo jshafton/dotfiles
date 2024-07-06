d1d402927249c11b3be57903c7e2d31fae22b8b2
Jacob Shafton <jacob@shafton.com>
2024-07-06 10:12:05 -0600

    wezterm: hide tab bar if only one tab

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,6 +9,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "nightfox"
 config.window_background_opacity = 0.95
 config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
 
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.font_size = 12.5
