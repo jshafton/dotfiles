5aaa0cd9c5dfaa46c8ee06bd4a5174f8cecbf840
Jacob Shafton <jacob@shafton.com>
2025-08-30 07:49:52 -0600

    ssh: clean up tmux / term handling

--- shellrc/rc.d.osx/ssh.osx.sh
+++ shellrc/rc.d.osx/ssh.osx.sh
@@ -1,25 +1,27 @@
-#! /usr/bin/env sh
+#!/usr/bin/env sh
 
-SCRIPT=$(cat <<'EOF'
-  if hash tmux-next >/dev/null; then
-    tmux="tmux-next"
-  else
-    tmux="tmux"
+# Create the remote script that will run on the server
+SCRIPT='
+if command -v tmux-next >/dev/null 2>&1; then
+  exec tmux-next new-session -A -s remote
+elif command -v tmux >/dev/null 2>&1; then
+  exec tmux new-session -A -s remote
+fi
+'
+
+# Define the ssh wrapper function
+ssh() {
+  local term_override=""
+
+  # Handle kitty terminal compatibility
+  if [ "$TERM" = "xterm-kitty" ]; then
+    term_override="TERM=xterm-256color"
   fi
 
-  $tmux new -A -s remote
-EOF
-)
-
-if [ "$TERM" = "xterm-kitty" ]; then
-  # https://wiki.archlinux.org/title/Kitty#Terminal_issues_with_SSH
-  ssh() {
-    # currently broken with tmux
-    # kitty +kitten ssh -t "$@" "$SCRIPT"
-    TERM=xterm-256color /usr/bin/ssh -t "$@" "$SCRIPT"
-  }
-else
-  ssh() {
-    /usr/bin/ssh -t "$@" "$SCRIPT"
-  }
-fi
+  # Execute SSH with the script, but don't fail if tmux isn't available
+  if [ -n "$term_override" ]; then
+    env $term_override /usr/bin/ssh -t "$@" "bash -c '$SCRIPT' || true; exec \$SHELL -l"
+  else
+    /usr/bin/ssh -t "$@" "bash -c '$SCRIPT' || true; exec \$SHELL -l"
+  fi
+}
