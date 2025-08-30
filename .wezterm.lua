8725b3c4bd0f50924db82de27ca2db823768613d
Jacob Shafton <jacob@shafton.com>
2025-08-30 07:50:18 -0600

    wezterm: full opacity and bigger font

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,21 +7,21 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.color_scheme = "nightfox"
-config.window_background_opacity = 0.95
+config.window_background_opacity = 1.0
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 
 config.font = wezterm.font("VictorMono Nerd Font Mono")
-config.font_size = 12.5
+config.font_size = 13.0
 
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = true
 
 config.keys = {
-	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
-	-- Make Option-Right equivalent to Alt-f; forward-word
-	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+  -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+  { key = "LeftArrow",  mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  { key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
 }
 
 -- and finally, return the configuration to wezterm
