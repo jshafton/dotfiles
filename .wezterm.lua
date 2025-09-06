e3e226e4ca7cae5c3cb3e327babf6e5711701eae
Jacob Shafton <jacob@shafton.com>
2025-09-06 10:07:22 -0600

    wezterm: theme/font changes

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,15 +4,21 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+config.color_scheme = "Sonokai (Gogh)"
+-- other color schemes:
+-- config.color_scheme = "Gruvbox Material (Gogh)"
+-- config.color_scheme = "nightfox"
 
-config.color_scheme = "nightfox"
 config.window_background_opacity = 1.0
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 
-config.font = wezterm.font("VictorMono Nerd Font Mono")
-config.font_size = 13.0
+config.font = wezterm.font("JetBrains Mono", { weight = 'DemiLight' }) -- VictorMono Nerd Font Mono
+-- other fonts:
+-- config.font = wezterm.font("VictorMono Nerd Font Mono", { weight = 'Medium' })
+
+config.font_size = 12.5
+config.line_height = 1.05
 
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = true
