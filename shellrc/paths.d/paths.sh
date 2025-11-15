4e5388f350291278421f9bf98fec13758e7430bc
Jacob Shafton <jacob@shafton.com>
2025-11-15 09:20:23 -0700

    paths: add ~/.local/bin

--- shellrc/paths.d/paths.sh
+++ shellrc/paths.d/paths.sh
@@ -1,6 +1,10 @@
 # Add `~/bin` to the `$PATH`
 path_prepend "$HOME/bin"
 
+if [ -d "$HOME/.local/bin" ]; then
+  path_prepend "$HOME/.local/bin"
+fi
+
 if [ -d "$HOME/.rd/bin" ]; then
   path_prepend "$HOME/.rd/bin"
 fi
